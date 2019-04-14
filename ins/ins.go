0e8ddef5aeb9cee584167368a252dbaf040eac64
vmiclott <victor.miclotte@gmail.com>
2019-04-14 19:33:32 +0200

    ins solved.

---
+++ ins/ins.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*TODO: Hard-coded input needs to go*/
+	n := 6
+	a := []int{6, 10, 4, 5, 1, 2}
+	fmt.Println(countSwaps(n, a))
+	fmt.Println(a)
+}
+
+func countSwaps(n int, a []int) int {
+	count := 0
+	for i := 1; i < n; i++ {
+		k := i
+		for k > 0 && a[k] < a[k-1] {
+			temp := a[k-1]
+			a[k-1] = a[k]
+			a[k] = temp
+			k = k - 1
+			count = count + 1
+		}
+	}
+	return count
+}
